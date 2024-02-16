c6052c258d4defbc746689ded038659b98e890ca
Mustafa Cakir <98mustafacan@gmail.com>
2024-02-16 21:20:27 +0300

    Initial Commit

---
+++ EarthQuakeSSC/EarthQuakeSSCApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthQuakeSSCApp.swift
+//  EarthQuakeSSC
+//
+//  Created by MustafaCan on 16.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct EarthQuakeSSCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
